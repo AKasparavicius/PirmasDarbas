b9a1f4dbe9e949a7f4921ecbd953712ed5ad6112
AKasparavicius <kasparavicius.ar@gmail.com>
2023-03-26 20:41:06 +0300

    Assign max values to variables

--- app/src/main/java/lt/arnas/pirmasdarbas/MainActivity.kt
+++ app/src/main/java/lt/arnas/pirmasdarbas/MainActivity.kt
@@ -2,11 +2,28 @@ package lt.arnas.pirmasdarbas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+val TAG = "my_epic_tag"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        doSumOfMaxValues()
+    }
+
+    fun doSumOfMaxValues() {
+//    Priskirkite max vertes kintamiesiems
+        var maxByte: Byte = Byte.MAX_VALUE
+        var maxShort: Short = Short.MAX_VALUE
+        var maxInt: Int = Int.MAX_VALUE
+        var maxLong: Long = Long.MAX_VALUE
+
+        Log.i(
+            TAG, "maxByte: $maxByte, maxShort: $maxShort, maxInt: $maxInt" +
+                    ", maxLong: $maxLong"
+             )
     }
 }
